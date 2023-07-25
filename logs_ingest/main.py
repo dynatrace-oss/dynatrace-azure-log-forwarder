f44a855014f0ee87e49ddd3a81d38c7785ee2794
NematulloKozimov <110405489+NematulloKozimov@users.noreply.github.com>
2023-07-25 09:29:14 +0200

    Show size of eventHubEvent main.py

--- logs_ingest/main.py
+++ logs_ingest/main.py
@@ -53,6 +53,7 @@ def main(events: List[func.EventHubEvent]):
 
 
 def process_logs(events: List[func.EventHubEvent], self_monitoring: SelfMonitoring):
+    print(f"eventLength: {events.length}")
     try:
         verify_dt_access_params_provided()
         logging.throttling_counter.reset_throttling_counter()
