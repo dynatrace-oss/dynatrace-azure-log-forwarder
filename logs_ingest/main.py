67f2dde23c2450ad0c6b87619c063a6789c36589
NematulloKozimov <110405489+NematulloKozimov@users.noreply.github.com>
2023-08-04 08:52:53 +0200

    Update main.py

--- logs_ingest/main.py
+++ logs_ingest/main.py
@@ -109,8 +109,8 @@ def extract_logs(events: List[func.EventHubEvent], self_monitoring: SelfMonitori
         records = event_json.get("records", [])
         # print(f"recordListLength: {len(records)}")
 
-        logs_to_be_sent_to_dt = pool.map(extract_dt_partial, records)
-        logs_to_be_sent_to_dt = [data for data in logs_to_be_sent_to_dt if data]
+        results = pool.map(extract_dt_partial, records)
+        logs_to_be_sent_to_dt.extend([data for data in result if data])
         
         # for record in records:
         #     try:
