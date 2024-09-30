ee223b4e557e134f5df57505bcb21f51c924fc7f
PatrykOzogDynatrace <patryk.ozog@dynatrace.com>
2024-09-30 12:10:18 +0200

    wip

--- logs_ingest/monitored_entity_id.py
+++ logs_ingest/monitored_entity_id.py
@@ -60,10 +60,11 @@ def infer_monitored_entity_id(category: str, parsed_record: Dict):
         identifier = [create_monitored_entity_id(dt_me_type_element, resource_id) for dt_me_type_element in dt_me_type]
         print("identifier", identifier)
         parsed_record["dt.entity.azure_function_app"] = identifier[0]
-        dt_me_type_casefold = [element.casefold() for element in dt_me_type]
-        if CUSTOM_DEVICE_ENTITY_TYPE.casefold() in dt_me_type_casefold:
-            index = dt_me_type_casefold.index(CUSTOM_DEVICE_ENTITY_TYPE.casefold())
-            parsed_record["dt.entity.custom_device"] = identifier[index]
+        parsed_record["dt.entity.azure_function_app2"] = identifier[-1]
+        # dt_me_type_casefold = [element.casefold() for element in dt_me_type]
+        # if CUSTOM_DEVICE_ENTITY_TYPE.casefold() in dt_me_type_casefold:
+        #     index = dt_me_type_casefold.index(CUSTOM_DEVICE_ENTITY_TYPE.casefold())
+        #     parsed_record["dt.entity.custom_device"] = identifier[index]
         # if dt_me_type.casefold() == CUSTOM_DEVICE_ENTITY_TYPE.casefold():
         #     parsed_record["dt.entity.custom_device"] = identifier
 
